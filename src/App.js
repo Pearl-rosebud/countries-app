0e63ab27747d1c00793ef49738a9bd3fa1003dd0
Pearl-rosebud <110114349+Pearl-rosebud@users.noreply.github.com>
2022-08-24 11:14:26 -0700

    more routes added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import AboutScreen from './screens/AboutScreen';
-import DetailScreen from './screens/DetailScreen';
-import ContactScreen from './screens/ContactScreen';
-import Header from './components/Header';
-
-
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ContactScreen from "./screens/ContactScreen";
+import AboutScreen from "./screens/AboutScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <Header/>
+        <Header />
         <Routes>
           <Route path="/" element={<HomeScreen />} />
           <Route path="/contact" element={<ContactScreen />} />
           <Route path="/about" element={<AboutScreen />} />
-          <Route path="/detail:name" element={<DetailScreen />} />
+          <Route path="/product" element={<ProductScreen />} />
         </Routes>
       </div>
     </BrowserRouter>
